2c4bcc5e4cc7c26021ae2b9958ee2c40a8047717
Colton Horvath <coltonrhorvath@gmail.com>
2018-05-24 13:39:01 -0400

    Adding textures & small code smaples

---
+++ Swarm/Assets/Colton.cs
@@ -0,0 +1,35 @@
+﻿
+using UnityEngine;
+
+public class Colton : MonoBehaviour {
+
+    public Rigidbody God;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetKey("w"))
+        {
+            God.AddForce(5000 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("s"))
+        {
+            God.AddForce(-5000 * Time.deltaTime, 0, 0);
+        }
+        if (Input.GetKey("a"))
+        {
+            God.AddForce(0, 0, 5000 * Time.deltaTime);
+        }
+        if (Input.GetKey("d"))
+        {
+            God.AddForce(0, 0, -5000 * Time.deltaTime);
+        }
+        if (Input.GetKey("space"))
+        {
+            God.AddForce(0, 5000 * Time.deltaTime, 0);
+        }
+    }
+}
