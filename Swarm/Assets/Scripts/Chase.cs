4164d784afc6e4fa09c148608344f856f67a9a87
Colton Horvath <coltonrhorvath@gmail.com>
2018-05-25 00:50:40 -0400

    Issues with AI

---
+++ Swarm/Assets/Scripts/Chase.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chase : MonoBehaviour {
+
+    public Transform Human;
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+        if (Vector3.Distance(Human.position, this.transform.position) < 40)
+        {
+            Vector3 direction = Human.position - this.transform.position;
+        }
+    }
+}
---
+++ Swarm/Assets/Scripts/InfectionController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class InfectionController : MonoBehaviour {
+
+    public float searchRadius = 20f;
+    [SerializeField]
+    Transform huntedTarget;
+    NavMeshAgent agent;
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, searchRadius);
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+        agent = this.GetComponent<NavMeshAgent>();
+	}
+
+    public Transform Target;
+	// Update is called once per frame
+	void Update () {
+
+        Hunt();
+        
+        /*if (Vector3.Distance(Target.position, this.transform.position) < 20)
+        {
+            Vector3 huntTarget = huntedTarget.transform.position;
+            agent.SetDestination(huntTarget);
+            //Vector3 direction = Target.position - this.transform.position;
+        }*/
+
+	}
+
+    private void Hunt()
+    {
+        if (Vector3.Distance(Target.position, this.transform.position) < 20)
+        {
+            Vector3 huntTarget = GameObject.FindWithTag("Human").transform.position;
+            agent.SetDestination(huntTarget);
+            Vector3 direction = GameObject.FindWithTag("Human").transform.position - this.transform.position;
+        }
+    }
+}
